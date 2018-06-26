d26ac4c7e9aac5f9dd1381a65ff97413a0dfd46e
HerbL27 <bettercodes1@gmail.com>
2018-06-26 12:20:06 +0100

    Added more needed filetypes

--- Web Crawler/Utilities/FileTypes.cs
+++ Web Crawler/Utilities/FileTypes.cs
@@ -5,13 +5,13 @@ namespace Web_Crawler.Utilities
     class FileTypes
     {
         public static List<string> All = new List<string>();
-        public static List<string> Video { get; } = new List<string>() { "M2TS", "MP4", "MKV", "AVI", "MPEG", "MPG", "MOV", "M4V" };
+        public static List<string> Video { get; } = new List<string>() { "M2TS", "MP4", "MKV", "AVI", "MPEG", "MPG", "MOV", "M4V", "WMV", "FLV" };
         public static List<string> Audio { get; } = new List<string>() { "MP3", "WMA", "WAV", "M3U", "APE", "AIF", "MPA", "CDA", "AC3", "OGG", "FLAC", "M4A" };
-        public static List<string> Image { get; } = new List<string>() { "TIFF", "TIF", "JPEG", "JPG", "BMP", "GIF", "PNG", "EPS", "RAW", "SVG" };
+        public static List<string> Image { get; } = new List<string>() { "ICO", "TIFF", "TIF", "JPEG", "JPG", "BMP", "GIF", "PNG", "EPS", "RAW", "SVG" };
         public static List<string> Book { get; } = new List<string>() { "MOBI", "CBZ", "CBR", "CBC", "CHM", "EPUB", "FB2", "LIT", "LRF", "ODT", "PDF", "PRC", "PDB", "PML", "RB", "RTF", "TCR", "DOC", "DOCX" };
         public static List<string> Software { get; } = new List<string>() { "EXE", "VOB", "ZIP", "TAR", "RAR", "7Z", "ISO", "PKG", "TAR.GZ", "APK", "IPA", "APPX", "XAP", "JAR" };
         public static List<string> Torrent { get; } = new List<string>() { "TORRENT" };
         public static List<string> Subtitle { get; } = new List<string>() { "SRT", "SUB", "VTT" };
-        public static List<string> Other { get; } = new List<string>() { "NFO", "MQ4", "NDS", "JSP", "GG", "SWF", "PS", "RTF", "BAS", "CC", "C", "CPP", "CXX", "H", "HPP", "CS", "JAVA", "PY", "PL", "PHP", "DB", "HTM", "HTML", "ASPX", "XML", "TXT", "SQL", "CSV", "PSD", "GZ", "BIN", "PAR", "PAR2", "PYK", "PK3", "PK4", "SKB", "IMG", "HGZ", "AI", "OTF", "TTF", "CSS", "PLS", "Z64" };
+        public static List<string> Other { get; } = new List<string>() { "DAT", "INI", "ASM", "MD", "DLL", "SFV", "CPK", "RPT", "CAB", "BAT", "URL", "CF1", "CF2", "INF", "NFO", "MQ4", "NDS", "JSP", "GG", "SWF", "PS", "RTF", "BAS", "CC", "C", "CPP", "CXX", "H", "HPP", "CS", "JAVA", "PY", "PL", "PHP", "DB", "HTM", "HTML", "ASPX", "XML", "TXT", "SQL", "CSV", "PSD", "GZ", "BIN", "LOG", "CUE", "PAR", "PAR2", "PYK", "PK3", "PK4", "SKB", "IMG", "HGZ", "AI", "OTF", "TTF", "CSS", "PLS", "Z64" };
     }
 }
